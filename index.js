6aae8b9c9d4a2e6191e4ed81490f7490aa7d346f
ClaudioFritzen <sdfritzen96@gmail.com>
2022-12-10 23:22:53 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const cursos = ['FullStack Master', 'Desenvolvimento de Games', 'Viver de Youtube'];
+
+
+// Retornar um curso
+server.get('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(cursos[index]);
+});
+
+// Retornar todos os cursos
+server.get('/cursos', (req, rest) => {
+    return res.json(cursos);
+})
+
+// Criar um novo curso
+server.post('/cursos', (req, res) => {
+    const { name} = req.body;
+    cursos.push(name);
+
+    return rs.json(cursos);
+});
+
+// atualizar um curso
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+// excluir um curso
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params
+
+    cursos.splice(index, 1);
+    return res.json({ message: " O curso foi deletado "})
+});
+
+
+
+
+
+
+
+server.listen(3000);
